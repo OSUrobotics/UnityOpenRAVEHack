e1a495560b8e3b624732d297daf8d53a0980e51a
Ammar Kothari <ammar.kothari@gmail.com>
2017-12-13 11:27:14 -0800

    Table with objects scene is set up

---
+++ UnityOpenRAVE/Assets/Scripts/ObjectGravity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	public Collider coll;
+	void Start() {
+		coll = GetComponent<Collider>();
+		coll.isTrigger = true;
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.attachedRigidbody)
+			other.attachedRigidbody.useGravity = false;
+		
+	}
+}
