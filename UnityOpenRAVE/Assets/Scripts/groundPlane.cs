ef43ccccf7ebc48c8762481c916b0469c56e9f8a
Ammar Kothari <ammar.kothari@gmail.com>
2018-01-20 16:01:17 -0800

    screen shot still doesn't work

---
+++ UnityOpenRAVE/Assets/Scripts/groundPlane.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundPlane : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("Object Entered the Trigger");
+	}
+	
+//	void OnTriggerStay (Collider other)
+//	{
+//		Debug.Log ("Object is within Trigger");
+//	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		Debug.Log ("Object Exited the Trigger");
+	}
+}
